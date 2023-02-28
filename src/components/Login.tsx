c6d0d02703dbe7cb5a671d107b6f289014ac6a9a
Gabor <email@example.com>
2023-02-28 12:02:24 +0100

    Login (Kell még a backend)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,29 @@
+import { Component } from "react";
+import LoginForm from "./LoginForm";
 
+interface State {
+    authToken: string;
+}
 
-export default () => <p>Login</p>;
+export default class Login extends Component<{}, State> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            authToken: '',
+        }
+    }
+
+    render() {
+        const { authToken } = this.state;
+        const loggedIn = authToken != '';
+
+        return <div>
+            <LoginForm
+            authToken={authToken}
+            onAuthTokenChange={(token) => this.setState({ authToken: token })}
+            />
+        </div>
+    }
+    
+}
